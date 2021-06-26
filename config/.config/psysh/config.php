ee4432b488b7b44a397c329216fb3c5dc83240f6
Jordan Brauer <jordan.brauer@gubagoo.com>
2021-06-25 20:30:42 -0500

    load project code if found

--- config/.config/psysh/config.php
+++ config/.config/psysh/config.php
@@ -2,6 +2,14 @@
 
 declare(strict_types = 1);
 
+static $autoloader;
+
+$autoloader = sprintf('%s/vendor/autoload.php', getcwd());
+
+if (is_file($autoloader)) {
+    require_once $autoloader;
+}
+
 return [
     'prompt' => 'λ ',
 ];
