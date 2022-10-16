6a9658fa4adf89b722b2bc764de099f0f00773c9
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2022-10-16 17:18:26 -0500

    rename file and translate from VimScript to Lua

---
+++ config/.config/nvim/after/plugin/repl.lua
@@ -0,0 +1,10 @@
+vim.g['codi#interpreters'] = {
+    php = {
+        bin = 'psysh',
+        prompt = '^(λ|...) ',
+    },
+    ts = {
+        bin = 'tsun',
+        prompt = '^(>|...) ',
+    },
+}
