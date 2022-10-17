9d0395ba1767bfd49b2024e91657297617756a21
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2022-10-16 19:23:38 -0500

    ignore editorconfig formatting for filetypes

--- config/.config/nvim/after/plugin/lsp.lua
+++ config/.config/nvim/after/plugin/lsp.lua
@@ -10,6 +10,8 @@ local servers = {
     'elixirls',
 }
 
+vim.g.EditorConfig_exclude_patterns = { 'fugitive://.*', 'scp://.*' }
+
 -- Use an on_attach function to only map the following keys 
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
