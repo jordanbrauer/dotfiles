5c04d9fff8b2970c984364191eb00f4c10ed7fee
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2022-10-16 19:23:11 -0500

    re-add keymap to view syntax highlight group under cursor

--- config/.config/nvim/after/plugin/treesitter.lua
+++ config/.config/nvim/after/plugin/treesitter.lua
@@ -12,3 +12,15 @@ require 'nvim-treesitter.configs'.setup {
         "php", "typescript", "go", "gomod", "graphql", "javascript"
     },
 }
+
+-- Show (non-Treesitter) syntax highlighting groups for word under cursor.
+vim.keymap.set(
+    'n',
+    'gm',
+    function()
+        local selected = vim.fn.synID(vim.fn.line('.'), vim.fn.col('.'), 1)
+        local child = vim.fn.synIDattr(selected, 'name')
+        local parent = vim.fn.synIDattr(vim.fn.synIDtrans(selected), 'name')
+
+        print(string.format('Syntax Group: %s → %s', child, parent))
+    end)
