472fccc9acaeae048cb868382434b617f8dce0bc
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2022-10-16 20:54:17 -0500

    adding keymaps for snippets

--- config/.config/nvim/after/plugin/keymap.lua
+++ config/.config/nvim/after/plugin/keymap.lua
@@ -7,3 +7,12 @@ vim.keymap.set('n', '<C-l>', '<C-W>l')
 -- Move selected lines with shift+<JK>
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
+
+-- Use <Tab> and <S-Tab> to navigate through popup menus
+-- QUESTION: are these needed anymore?
+-- vim.keymap.set('i', '<Tab>', function() if vim.fn.pumvisible() then return '<C-n>' else return '<Tab>' end end, { noremap = true, expr = true })
+-- vim.keymap.set('i', '<S-Tab>', function() if vim.fn.pumvisible() then return '<C-p>' else return '<S-Tab>' end end, { noremap = true, expr = true })
+
+-- Harpoon
+vim.keymap.set('n', '<leader>c', require('harpoon.ui').toggle_quick_menu, { silent = true, noremap = true })
+vim.keymap.set('n', '<leader>a', require('harpoon.mark').add_file, { silent = true, noremap = true })
